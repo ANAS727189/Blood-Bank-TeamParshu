dbdf4450c72324053cc5496a68f2df5f08d8f11b
Shardendu Mishra <shardendumishrabrave@gmail.com>
2025-01-31 20:32:47 +0530

    implemented user, admin, organisation and admin controllers and middlewares

--- server/route/SourceRoutes/admin.route.ts
+++ server/route/SourceRoutes/admin.route.ts
@@ -1,11 +1,38 @@
-import { login, register } from '../../controller/admin.controller';
+import { 
+  deleteDonationLocation,
+  getDonationLocations,
+  deleteOrganisation,
+  deleteBloodRequest,
+  getBloodRequests,
+  getOrganistion,
+  deletePatient,
+  getAnalytics,
+  deleteDonor,
+  getPatients,
+  getDonors,
+  register, 
+  login,
+} from '../../controller/admin.controller';
+
 import { Router } from 'express';
+import { adminMiddleware } from '../../middleware/admin.middleware';
 const router = Router();
 
 // Routes Go Here
 
-router.post('/register', register);
 router.post('/login', login);
+router.post('/register', register);
+router.get('/getDonors',adminMiddleware ,getDonors);
+router.get('/getPatients',adminMiddleware ,getPatients);
+router.get('/getAnalytics',adminMiddleware ,getAnalytics);
+router.delete('/deleteDonor',adminMiddleware ,deleteDonor);
+router.get('/getOrganistion',adminMiddleware ,getOrganistion);
+router.delete('/deletePatient',adminMiddleware ,deletePatient);
+router.get('/getBloodRequests',adminMiddleware ,getBloodRequests);
+router.delete('/deleteOrganisation',adminMiddleware ,deleteOrganisation);
+router.get('/getDonationLocations',adminMiddleware ,getDonationLocations);
+router.delete('/deleteDonationLocation',adminMiddleware ,deleteDonationLocation);
+router.delete('/deleteBloodRequest',adminMiddleware ,deleteBloodRequest);
 
 // Routes Go Here
 
--- server/route/SourceRoutes/patient.route.ts
+++ server/route/SourceRoutes/patient.route.ts
@@ -1,11 +1,22 @@
-import { login, register } from '../../controller/patient.controller';
+import {   
+  login,
+  register, 
+  getBloodRequests,
+  getBloodAvailable,
+  deleteBloodRequest 
+} from '../../controller/patient.controller';
+
 import { Router } from 'express';
+import { patientMiddleware } from '../../middleware/patient.middleware';
 const router = Router();
 
 // Routes Go Here
 
-router.post('/register', register);
 router.post('/login', login);
+router.post('/register', register);
+router.get('/bloodRequests',patientMiddleware, getBloodRequests);
+router.get('/bloodAvailable',patientMiddleware, getBloodAvailable);
+router.delete('/bloodRequest/:requestId',patientMiddleware, deleteBloodRequest);
 
 // Routes Go Here
 
